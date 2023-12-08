278b9626976093f6b46acf08065998c4489e094f
Sanriku-Gaming <100663906+Sanriku-Gaming@users.noreply.github.com>
2023-12-07 21:10:53 -0700

    rewrite to include config, job check and qb-input menu

--- server.lua
+++ server.lua
@@ -1,35 +1,54 @@
-local QBCore = exports["qb-core"]:GetCoreObject()
-local PlayerData = {}
+local QBCore = exports['qb-core']:GetCoreObject()
 
-RegisterServerEvent('teleportPlayerToJail')
-AddEventHandler('teleportPlayerToJail', function(playerId, cmd, time)
-    local x, y, z, heading = 0, 0, 0, 0 -- Coordenadas iniciales (se sobrescribirán)
-    local x2, y2, z2, heading2 = 0, 0, 0, 0 -- Coordenadas después de 'time' segundos
+QBCore.Functions.CreateCallback('localPrison:server:nearbyPlayers', function(source, cb)
+    local Player = QBCore.Functions.GetPlayer(source)
+    local nearbyPlayers = {}
+    for k, v in pairs(QBCore.Functions.GetPlayers()) do
+        local nPlayer = QBCore.Functions.GetPlayer(v)
+        if #(GetEntityCoords(GetPlayerPed(source)) - GetEntityCoords(GetPlayerPed(v))) < 5 then
+            if #(GetEntityCoords(GetPlayerPed(source)) - GetEntityCoords(GetPlayerPed(v))) ~= 0 then
+                nearbyPlayers[#nearbyPlayers+1] = {
+                    value = v,
+                    text = nPlayer.PlayerData.charinfo.firstname..' ' ..nPlayer.PlayerData.charinfo.lastname,
+                }
+            end
+        end
+    end
+    cb(nearbyPlayers)
+end)
 
-    if cmd == 'jail1' then
-        x, y, z, heading = -429.69, 6001.41, 31.72, 131.24 -- Coordenadas jail1  
-        x2, y2, z2, heading2 = -438.07, 6018.17, 31.49, 317.68 -- Coordenadas después de 'time' segundos vector4()
-    elseif cmd == 'jail2' then
-        x, y, z, heading = -426.59, 5998.07, 31.72, 132.37 -- Coordenadas jail2
-        x2, y2, z2, heading2 = -465.31, 6042.85, 31.34, 0 -- Coordenadas después de 'time' segundos
-    elseif cmd == 'jail3' then
-        x, y, z, heading = 1845.41, 3684.74, 34.26, 299.21 -- Coordenadas jail3
-        x2, y2, z2, heading2 = 1854.21, 3681.02, 34.27, 220.34 -- Coordenadas después de 'time' segundos
-    elseif cmd == 'jail4' then
-        x, y, z, heading = 1843.71, 3687.82, 34.26, 304.08 -- Coordenadas jail4
-        x2, y2, z2, heading2 = 1854.21, 3681.02, 34.27, 220.34 -- Coordenadas después de 'time' segundos
+QBCore.Commands.Add('localJail', 'Place player in local cell', {}, false, function(source)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    if Player.PlayerData.job.type == 'leo' and Player.PlayerData.job.onduty then
+        TriggerClientEvent('localPrison:client:jailInput', src)
     else
-        print(('Comando desconocido: %s'):format(cmd))
-        return
+        TriggerClientEvent('QBCore:Notify', src, 'Only LEO jobs can use this command', 'error')
+    end
+end)
+
+RegisterServerEvent('localPrison:server:jailPlayer', function(jailed)
+    local src = source
+    local crim = tonumber(jailed.crim)
+    local cell = tonumber(jailed.cell)
+    local time = tonumber(jailed.time)
+    local Player = QBCore.Functions.GetPlayer(src)
+    if not Player then return end
+
+    local OtherPlayer = QBCore.Functions.GetPlayer(crim)
+    if not OtherPlayer then return end
+    local otherName = OtherPlayer.PlayerData.charinfo.firstname..' ' ..OtherPlayer.PlayerData.charinfo.lastname,
+
+    local currentDate = os.date('*t')
+    if currentDate.day == 31 then
+        currentDate.day = 30
     end
 
-    if time > 0 then
-        print(('Jugador %s teletransportado a la coordenada %s por %s segundos.'):format(playerId, cmd, time))
-        TriggerClientEvent('teleportToCoords', playerId, x, y, z)
-        Citizen.Wait(time * 100000) -- Esperar 'time' segundos en milisegundos
-        print(('Tiempo cumplido. Jugador %s teletransportado a la siguiente coordenada.'):format(playerId))
-        TriggerClientEvent('teleportToCoords', playerId, x2, y2, z2)
-    else
-        print(('Error: El tiempo debe ser mayor que cero para el jugador %s.'):format(playerId))
-    end
+    OtherPlayer.Functions.SetMetaData('injail', time)
+    OtherPlayer.Functions.SetMetaData('criminalrecord', {
+        ['hasRecord'] = true,
+        ['date'] = currentDate
+    })
+    TriggerClientEvent('localPrison:client:sendToCell', OtherPlayer.PlayerData.source, time)
+    TriggerClientEvent('QBCore:Notify', src, otherName .. ' has been sent to Cell #' .. cell .. ' for ' .. time .. ' minutes.')
 end)
